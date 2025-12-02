47091b8cd9baf1850ba14e84fb3fd889dd53ea1e
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2025-12-01 18:15:52 -0800

    day1

---
+++ day1/part1.py
@@ -0,0 +1,26 @@
+def solve():
+    with open('input.txt', 'r') as file:
+        content = file.read()
+        steps = content.split()
+        
+        current_position = 50
+        
+        zero_click = 0
+        
+        for step in steps:
+            direction = step[0]
+            distance = int(step[1:])
+                        
+            if direction == 'L':
+                current_position = (current_position - distance) % 100
+            else:
+                current_position = (current_position + distance) % 100
+                
+            if current_position == 0:
+                zero_click += 1
+        
+        print(zero_click)
+            
+    
+if __name__ == '__main__':
+    solve()    
---
+++ day1/part2.py
@@ -0,0 +1,28 @@
+def solve():
+    with open('input.txt', 'r') as file:
+        content = file.read()
+        steps = content.split()
+        
+        current_position = 50
+        zero_click = 0
+        
+        for step in steps:
+            direction = step[0]
+            distance = int(step[1:])
+            
+            sign = 1
+                
+            while distance > 0:
+                if direction == 'L':
+                    current_position -= 1
+                else:
+                    current_position += 1
+                if(current_position % 100 == 0):
+                    zero_click += 1
+                distance -= 1
+        
+        print(zero_click)
+            
+    
+if __name__ == '__main__':
+    solve()    
